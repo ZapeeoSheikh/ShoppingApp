a9f88537cbd910eaac5e444ad94f2d1b2d42bf2c
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-09 19:45:41 +0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ShoppingApp.MvcApplication" Language="C#" %>
---
+++ Models/ShoppingContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace ShoppingApp.Models
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext() : base("ConnectionString")
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderStatus> OrderStatus { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductStatus> ProductStatus { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Role> Roles { get; set; }
+    }
+}
