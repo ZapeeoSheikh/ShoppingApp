38f38afdeb59a281efdd4ee872227ffaea4b9e28
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-09 19:57:19 +0500

    upadte

--- Models/Role.cs
+++ Models/Role.cs
@@ -7,5 +7,7 @@ namespace ShoppingApp.Models
 {
     public class Role
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Models/User.cs
+++ Models/User.cs
@@ -7,5 +7,12 @@ namespace ShoppingApp.Models
 {
     public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public virtual Role Role { get; set; }
+        public int RoleId { get; set; }
+
     }
 }
