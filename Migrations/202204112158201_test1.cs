fcfb38940810146ea60c9af2fb04e9009f28fb84
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-12 02:58:59 +0500

    update db

---
+++ Migrations/202204112158201_test1.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+
+    public partial class test1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
