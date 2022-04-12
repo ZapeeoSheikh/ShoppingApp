25427adfaa2fa8730dab128de58e2c58dc262716
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-13 01:46:11 +0500

    order post

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -11,25 +11,57 @@ namespace ShoppingApp.Controllers
     {
         // GET: Order
         ShoppingContext db = new ShoppingContext();
+        [HttpGet]
         public ActionResult Order()
         {
             List<Order> order = db.Orders.ToList();
             return View(order);
         }
+        [HttpPost]
+        public ActionResult Order(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return Redirect("/Order/Order");
+        }
+        [HttpGet]
         public ActionResult OrderStatus()
         {
             List<OrderStatus> orderStatus = db.OrderStatus.ToList();
             return View(orderStatus);
         }
+        [HttpPost]
+        public ActionResult OrderStatus(OrderStatus orderStatus)
+        {
+            db.OrderStatus.Add(orderStatus);
+            db.SaveChanges();
+            return Redirect("/Order/OrderStatus");
+        }
+        [HttpGet]
         public ActionResult Role()
         {
             List<Role> roles = db.Roles.ToList();
             return View(roles);
         }
+        [HttpPost]
+        public ActionResult Role(Role role)
+        {
+            db.Roles.Add(role);
+            db.SaveChanges();
+            return Redirect("/Order/Role");
+        }
+        [HttpGet]
         public ActionResult User()
         {
             List<User> users = db.Users.ToList();
             return View(users);
+        }
+        [HttpPost]
+        public ActionResult User(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+            return Redirect("/Order/User");
         }
     }
 }
