cedd5ff9550eb950ac09714748ea63f6e3a789c2
ZapeeoSheikh <muhammadrmz871@gmail.com>
2022-04-13 01:58:48 +0500

    update

---
+++ Migrations/202204122058138_btest.cs
@@ -0,0 +1,16 @@
+﻿namespace ShoppingApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class btest : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
